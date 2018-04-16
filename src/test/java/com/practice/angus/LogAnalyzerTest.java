d4b12e26dd46a643daca03dd53404d55e8ee162b
AngusWu <anguswu@simbo.com.tw>
2018-04-16 14:12:27 +0800

    add good upper case extension return true

--- src/test/java/com/practice/angus/LogAnalyzerTest.java
+++ src/test/java/com/practice/angus/LogAnalyzerTest.java
@@ -3,8 +3,16 @@ package com.practice.angus;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LogAnalyzerTest {
+    @Test
+    public void IsValidFileName_GoodExtensionUppercase_ReturnsTrue() {
+        LogAnalyzer analyzer = new LogAnalyzer();
+        boolean result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+        assertTrue(result);
+    }
+
     @Test
     public void IsValidFileName_BadExtension_ReturnsFalse() {
         LogAnalyzer analyzer = new LogAnalyzer();
